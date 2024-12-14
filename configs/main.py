c391b8a3ebfef11607bd64dc465b3fc61b4e7408
Wissem. <wissem.badraoui@yahoo.com>
2024-12-14 19:34:28 +0100

    Application - Config & Console Methods

---
+++ configs/main.py
@@ -0,0 +1,12 @@
+import os
+import dotenv
+
+# Chargement du fichier d'environnement
+dotenv.load_dotenv()
+
+# Chargement des variables d'environnement
+CL_TOKEN = os.getenv("TOKEN")
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_NAME = os.getenv("DB_NAME")
--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-def init():
-    print("⚠️ Rubik's Network - Initial Testing")
+import discord
+import os
 
-if __name__ == '__main__':
-    init()
+from utils.console import Console
+from configs import main as configuration
+
+Console.info("Démarrage de l'application initialisé...")
+Client = discord.Bot()
+
+@Client.event
+async def on_ready():
+    Console.success(f"{Client.user} a été correctement démarré.")
+
+@Client.slash_command(name="hello", description="Say hello to the bot")
+async def hello(ctx: discord.ApplicationContext):
+    await ctx.respond("Hey!")
+
+Client.run(configuration.CL_TOKEN)
