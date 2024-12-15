8470ee9c577f90c2ab686576d8db604f6fe28db7
Wissem. <wissem.badraoui@yahoo.com>
2024-12-15 21:36:32 +0100

    Application - Structure Update

--- application/main.py
+++ application/main.py
@@ -5,33 +5,50 @@ from application.utils.logger import Logger
 from application.database.main import Database
 
 class Application(discord.Bot):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, client):
+        super().__init__(
+            intents=client.configuration['intents'],
+            activity=client.configuration['activity'],
+            status=client.configuration['status'],
+
+            loop=client.configuration['loop'],
+            proxy=client.configuration['proxy'],
+        )
 
         self.instance = super()
         self.database = Database(self)
-        Logger.info("Application initialisée. Chargement des modules...")
+
+        Logger.info("Démarrage de l'application en cours...")
 
     async def on_ready(self):
         """Événement déclenché lorsque l'application est prête."""
-        Logger.success(f"{self.user} a été correctement démarré.")
+        await self.database.connect()
         await self.load_modules()
+        Logger.success(f"{self.user} est prêt et opérationnel.")
 
     async def load_modules(self):
-        """Charge dynamiquement les modules (extensions)."""
-        for filename in os.listdir("./application/modules"):
-            if filename.endswith(".py"):
-                module_name = f"application.modules.{filename[:-3]}"  # Retire ".py"
-                try:
-                    # Charge chaque module individuellement
-                    await self.load_extension(module_name)
-                    Logger.success(f"Module {module_name} chargé avec succès.")
-                except Exception as e:
-                    Logger.error(f"Chargement du module '{module_name}' échoué : {e}")
+        """Charge dynamiquement les modules."""
+        Logger.info("Chargement des modules...")
+        try:
+            for filename in os.listdir("./application/modules"):
+                if filename.endswith(".py"):
+                    module_name = f"application.modules.{filename[:-3]}"
+                    try:
+                        self.load_extension(module_name)
+                        print(f"  → Module {module_name} chargé.")
+                    except Exception as error:
+                        Logger.warning(f"Chargement du module '{module_name}' échoué.\n{error}")
+
+            Logger.success("Tous les modules ont été correctement chargés.")
+
+        except Exception as error:
+            return Logger.error(f"Chargement des modules non réussi :\n{error}")
+
+    async def on_interaction(self, interaction: discord.Interaction):
+        if not await self.database.user.exists(interaction.user.id) and interaction.id != "caca":
+            return await interaction.message.channel.send("> Verification error")
 
     async def on_message(self, message: discord.Message):
-        if message.author == self.user:
-            return
-
-        if message.content.startswith("!ping"):
-            await message.channel.send("Pong!")
+        if message.author == self.user: return
+        if not await self.database.user.exists(message.author.id): return
+        await message.reply(message.content, mention_author=False)
